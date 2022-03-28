d170f16895740c81e53f1609c5e51d5f53f3281c
John <averagehero2@gmail.com>
2022-03-28 19:30:30 -0400

    Comments and accessibility update
    Make static methods private and change the class description

--- gatewayGrabber.py
+++ gatewayGrabber.py
@@ -1,46 +1,41 @@
 import subprocess, re
 
-class routerTester:
-    # REQS
+
+class RouterTester:
     '''
-    Locate router address
-    Grab address
-    Check validity
-        if not valid
-            ask user to verify
-    Return router address
+    Class to handle grabbing IP to router.
     '''
 
     def __init__(self):
-        self.localIP = self.defaultGrabber()
+        self.localIP = self.default_grabber()
 
     # Grabs default gateway
-    def defaultGrabber(self):
+    def default_grabber(self):
         commandOutput = str(subprocess.check_output('ipconfig'))
         if len(commandOutput) < 60:
-            self.informAndKillProcess(commandOutput)
+            self._informAndKillProcess(commandOutput)
         gatewayAddressLIST = (re.findall('Default Gateway\D+(\d+.\d+.\d+.\d+)', commandOutput))
         if len(gatewayAddressLIST)>1:
             print(">1 network adaptors found. VPN or equivalent might be running.\nRequesting user verification...")
             # I'm not sure how windows will order network adaptors.
             # On my machine, router address is the second match
-            localIP=gatewayAddressLIST[1]
-            localIP = self.IPVerification(localIP)
+            localIP = gatewayAddressLIST[1]
+            localIP = self._IPVerification(localIP)
         else:
-            localIP=gatewayAddressLIST[0]
+            localIP = gatewayAddressLIST[0]
             if len(localIP) != 11:
                 # need more data to see variances in router addresses. Verify until then.
                 print("WARNING: Suspicious IP: [%s]" % localIP)
-                localIP = self.IPVerification(localIP)
+                localIP = self._IPVerification(localIP)
         return localIP
 
-    def informAndKillProcess(self, commandOutput):
+    def _informAndKillProcess(self, commandOutput):
         print("ERROR: Unexpected response from command. Please screenshot this window and send to developer. Killing process...")
         print(commandOutput)
         input("Enter to continue...")
         exit(1)
 
-    def IPVerification(self, ip):
+    def _IPVerification(self, ip):
         if input("Old Gateway [%s]\nPlease confirm IP. Is this the IP you want to use? (y/n) " % ip).lower() == "n":
             cont = 'n'
             localIP = ip
